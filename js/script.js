87120c88e1957d36802a9e49eabd74e86e69d0b7
Florin Pop <popflorin1705@yahoo.com>
2017-01-17 09:23:26 +0200

    js toggleFixed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
+    var scrollFromTop = $(window).scrollTop();
+    var navbarHeight = $(".navbar").height();
+    
+    toggleFixed();
+    
     $(window).scroll(function (event) {
-        var scrollFromTop = $(window).scrollTop();
-        var navbarHeight = $(".navbar").height();
-        
+        scrollFromTop = $(window).scrollTop();
+        toggleFixed();
+    });
+    
+    function toggleFixed() {
         if(scrollFromTop > navbarHeight) {
             $(".navbar").addClass('fixed');
         } else {
             $(".navbar").removeClass('fixed');
         }
-    });
+    }
+    
 });
