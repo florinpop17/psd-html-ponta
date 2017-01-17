7b7848efb324f4f46412ea16e41d3c0336342c8d
Florin Pop <popflorin1705@yahoo.com>
2017-01-17 08:35:39 +0200

    scrollFromTop js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+    $(window).scroll(function (event) {
+        var scrollFromTop = $(window).scrollTop();
+        var navbarHeight = $(".navbar").height();
+        
+        if(scrollFromTop > navbarHeight) {
+            $(".navbar").css("position", "fixed");
+        } else {
+            $(".navbar").css("position", "static");
+        }
+    });
 });
