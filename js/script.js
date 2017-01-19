19b759b0fb3a48c3942e5f8e8bb739a8d94326fa
Florin Pop <popflorin1705@yahoo.com>
2017-01-19 13:13:17 +0200

    dynamic text header index

--- js/script.js
+++ js/script.js
@@ -16,4 +16,17 @@ $(document).ready(function(){
         }
     }
     
+    var arrText = ['.', '..', '...', 'leistungsfähig', 'leistungwillig', 'pragmatisch', 'commited', 'strategisch', 'beweglich', 'kreativ', 'schnell', 'oho' ];
+    var arrIndex = 0;
+    
+    var changingText = setInterval(function(){
+        if(arrIndex < arrText.length) {
+            
+            $("#added_text").html(arrText[arrIndex]);
+            
+            arrIndex++;
+        } else {
+            clearInterval(changingText);
+        }
+    }, 500);
 });
